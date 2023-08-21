abccc51da5c07ee283cdbb7b706b4f6b8f937ccd
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-08-21 21:50:07 +0530

    add all components

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -4,7 +4,7 @@ import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-const Provider = ({ children, session }: { session: Session, children:ReactNode }) => (
+const Provider = ({ children, session }: { session?: Session, children?:ReactNode }) => (
   <SessionProvider session={session}>{children}</SessionProvider>
 );
 
